d29c9d93e78bf566e7515171d2648d56480d718e
SeungYeonKo <createNewScene479@gmail.com>
2024-02-28 12:15:45 +0900

    애니메이션 블렌드트리, 애니메이션 레이어 및 가중치, 아바타 마스크

--- Assets/002. Script/Delegate/ActionExample.cs
+++ Assets/002. Script/Delegate/ActionExample.cs
@@ -18,22 +18,39 @@ public class ActionExample : MonoBehaviour
         // 반환 : 없음
         // 반드시 반환값이 없고, 매개 변수를 0~16개인 메서드를 담을 수 있는 대리자
         Action a = Eat;
-
+        a += () =>                   // "익명함수"라고도 부른다
+        {
+            Debug.Log("먹었다!");
+        };
+        a = delegate() 
+        {
+            Debug.Log("먹었다!");
+        };
         a();
         a.Invoke();
 
-        Action<string> b = Eat2;
+        Action<string> b = (foodName) =>
+        {
+            Debug.Log($"{foodName}을 먹었당~~");
+        };
+        // 람다식은 접근자, 함수이름, 반환문(return)이 없다.
+        // 코드를 더 가독성있게 만들거나 함수가 매개변수로 딱 한 번 쓰일 때 쓴다.
+
         b.Invoke("마라탕");
 
         // 매개변수를 최대 16개까지 입력 받을 수 있다
         // Action<string, int, float ...>
+
+        // 람다식이란 수학 함수를 하나의 식(코드 조각)으로 표현한 것
+        // F(x) = x + 3
+        // F(6) => 9
+        // F() = 4
+        // F(x, y) = x^2 + y^2
+        // F(2, 3) => 13
+        // F(string foodname) => Debug.Log($"{foodName}을 먹었당~~")
     }
     public void Eat()
     {
         Debug.Log("먹었당~~");
-    }
-    public void Eat2(string foodName)
-    {
-        Debug.Log($"{foodName}을 먹었당~~");
     }
 }
